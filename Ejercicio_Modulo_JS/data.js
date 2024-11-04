b962e3def4da2020e87b439ae01585e7eb259219
jogutierrez99 <64194024+jogutierrez99@users.noreply.github.com>
2024-11-04 19:46:19 +0100

    Añadir el data.js

---
+++ Ejercicio_Modulo_JS/data.js
@@ -0,0 +1,15 @@
+const products = [
+    { id: 1, name: "Peperomia caperata Rosso Mini", img: "./img/peperomia-caperata-rosso-mini.jpg", description: "Le gusta crecer en espacios de sombra o semisombra, mantener un sustrato húmedo.", price: 7, stock: 4 },
+    { id: 2, name: "Ceropegia Woodii", img: "./img/ceropegia-woodii.jpg", description: "Sus hojas en forma de corazón y su crecimiento la hacen ser la planta colgante más deseada.", price: 10, stock: 5 },
+    { id: 3, name: "musgo para terrario", img: "./img/musgo-para-terrario.jpg", description: "Muy exigente respecto al riego, necesita altos niveles de humedad para vivir feliz.", price: 5, stock: 4 },
+    { id: 4, name: "baobab mini", img: "./img/baobab-mini.jpg", description: "Sin duda, el árbol más exótico que puedes encontrar ¡y ahora disponible en tamaño mini!", price: 5, stock: 7 },
+    { id: 5, name: "senecio rowleyanus", img: "./img/senecio-rowleyanus.jpg", description: "Esta mini es bastante relajada y no pide demasiado: solo un poco de luz indirecta y riegos ocasionales.", price: 4, stock: 8 },
+    { id: 6, name: "asparagus plumosos mini", img: "./img/asparagus-plumosos-mini.jpg", description: "Esta planta trepadora es una planta perfecta para espacios de poca luz. Necesita riegos frecuentes.", price: 9, stock: 7 },
+    { id: 7, name: "Peperomia caperata Rosso Mini", img: "./img/peperomia-caperata-rosso-mini.jpg", description: "Le gusta crecer en espacios de sombra o semisombra, mantener un sustrato húmedo.", price: 7, stock: 4 },
+    { id: 8, name: "Ceropegia Woodii", img: "./img/ceropegia-woodii.jpg", description: "Sus hojas en forma de corazón y su crecimiento la hacen ser la planta colgante más deseada.", price: 10, stock: 5 },
+    { id: 9, name: "musgo para terrario", img: "./img/musgo-para-terrario.jpg", description: "Muy exigente respecto al riego, necesita altos niveles de humedad para vivir feliz.", price: 5, stock: 4 },
+    { id: 10, name: "baobab mini", img: "./img/baobab-mini.jpg", description: "Sin duda, el árbol más exótico que puedes encontrar ¡y ahora disponible en tamaño mini!", price: 5, stock: 7 },
+    { id: 11, name: "senecio rowleyanus", img: "./img/senecio-rowleyanus.jpg", description: "Esta mini es bastante relajada y no pide demasiado: solo un poco de luz indirecta y riegos ocasionales.", price: 4, stock: 8 },
+    { id: 12, name: "asparagus plumosos mini", img: "./img/asparagus-plumosos-mini.jpg", description: "Esta planta trepadora es una planta perfecta para espacios de poca luz. Necesita riegos frecuentes.", price: 9, stock: 7 }
+    // Más productos...
+  ];
